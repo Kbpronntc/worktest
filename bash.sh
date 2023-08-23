d8d5dd4f04dd34ba13bb700c97fa08bc2916fc18
Kbpronntc <97437980+Kbpronntc@users.noreply.github.com>
2023-08-23 22:30:30 +0700

    Add files via upload

--- bash.sh
+++ bash.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 HTTP_CODE=$(curl --write-out "%{http_code}\n" "https://yandex.ru" --output output.txt --silent)
 echo $HTTP_CODE 
+
+function testEndpointAvailability {
+    # -L is used to ensure cURL follows up to 30 status changes through to the end.
+    # Without the -L, cURL will stop at the Apache reverse-proxy 301 status instead of listening for 
+    # the eventual response.
+    endpointResponse=$(curl -oL --write-out '%{http_code}\n' --silent --head "${endpointToValidate}")
+
+    if ! [[ -z "${endpointResponse}" ]]
+    then
+        if ! [[ "${endpointResponse}" -eq "200" ]]
+        then
+            logThis "${tfAdminLogDir}/${tfAdminLogFile}" "HTTP Response was: ${endpointResponse}" "INFO"
+        else
+            logThis "${tfAdminLogDir}/${tfAdminLogFile}" "HTTP Response was OK: ${endpointResponse}" "INFO"
+        fi
+    fi
+}
