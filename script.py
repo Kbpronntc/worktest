5f5c1837fe8734e1d69c2875fc020895bbb3d185
Kbpronntc <97437980+Kbpronntc@users.noreply.github.com>
2023-09-04 18:21:10 +0700

    Update script.py

--- script.py
+++ script.py
@@ -3,14 +3,14 @@ import requests
 
 uri = "https://ya.ru/"
 
-result = requests.get(uri)
-print("Status")
-print(result.status_code)
-print()
-print("Result text")
-pprint(result.text)
+#result = requests.get(uri)
+#print("Status")
+#print(result.status_code)
+#print()
+#print("Result text")
+#pprint(result.text)
 # pprint(result.json())
 
 
 # with open("current_result.txt", "w", encoding="utf-8") as file:
-    file.write(result.text)
+    #file.write(result.text)
